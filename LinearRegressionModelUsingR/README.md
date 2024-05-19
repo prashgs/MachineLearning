05cfd1c03c85dbe0ab7fbb42b127122a2e41ce5d
prashgs <prashgs@users.noreply.github.com>
2024-05-18 22:11:43 -0400

    Updated

---
+++ LinearRegressionModelUsingR/README.md
@@ -0,0 +1,6 @@
+# Linear Model for Predicting House Prices
+
+
+Home prices are of obvious and immediate interest to all three of us from a personal and financial perspective. The real estate market has short- and long-term impacts on lifestyle and financial decisions we make for our families. Empirically, many variables have been used to price a house. Some common predictors are number of bedrooms or bathrooms, square footage, or year built. How do these and other predictors influence the price of a house? Could there be an interplay between predictors that would help with our prediction?
+
+Our housing data comes from Kaggle. This dataset contains house sale prices for King County, which includes Seattle. It includes homes sold between May 2014 and May 2015.
---
+++ LinearRegressionModelUsingR/project.R
@@ -0,0 +1,38 @@
+---
+  title: "Linear Model for Predicting House Prices"
+author: "Qi Chen, Prashanth Shankarappa, Matthew Smith"
+date: "7/29/2021"
+output: html_document
+---
+  
+  ```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Introduction
+
+Home prices are of obvious and immediate interest to all three of us from a personal and financial perspective. The real estate market has short- and long-term impacts on lifestyle and financial decisions we make for our families. Empirically, many variables have been used to price a house. Some common predictors are number of bedrooms or bathrooms, square footage, or year built. How do these and other predictors influence the price of a house? Could there be an interplay between predictors that would help with our prediction?
+  
+  ## Methods
+  
+  ### Cleaning the Data
+  
+  - Transform "year built" dates into "age"
+- na.omit
+
+### Data Exploration
+
+- Check for collinearity issues (Matt)
+- Ensure that LINE assumptions are met (Prashanth)
+
+
+
+- Influential points (Qi)
+
+### Model Building
+
+
+
+## Results
+
+## Discussion
